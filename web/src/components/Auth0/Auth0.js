86a60bf0bf17f6b91f1647c01b6aa52cea268999
A. David Thyresson <dthyresson@gmail.com>
2020-09-07 10:58:12 -0400

    Always use window.location.href for Auth0 redirect

--- web/src/components/Auth0/Auth0.js
+++ web/src/components/Auth0/Auth0.js
@@ -4,7 +4,7 @@ import { Auth0Client } from '@auth0/auth0-spa-js'
 const auth0 = new Auth0Client({
   domain: process.env.AUTH0_DOMAIN,
   client_id: process.env.AUTH0_CLIENT_ID,
-  redirect_uri: process.env.AUTH0_REDIRECT_URI,
+  redirect_uri: window.location.href,
   // ** NOTE ** Storing tokens in browser local storage provides persistence across page refreshes and browser tabs.
   // However, if an attacker can achieve running JavaScript in the SPA using a cross-site scripting (XSS) attack,
   // they can retrieve the tokens stored in local storage.
