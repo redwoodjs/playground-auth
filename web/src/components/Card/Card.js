d0ad09d21ab19f18a77b0d02ffd33e36e00c532e
Morgan Spencer <morgan@spencercreative.co>
2021-04-17 17:05:52 -0400

    Delete card component

--- web/src/components/Card/Card.js
+++
@@ -1,9 +0,0 @@
-const Card = ({ children }) => {
-  return (
-    <li className="bg-white shadow-lg overflow-hidden rounded-md px-6 py-4">
-      {children}
-    </li>
-  )
-}
-
-export default Card
