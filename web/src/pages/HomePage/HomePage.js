221b7ebab328fea4fa6cd84d67efefdbfb0f8cac
Morgan Spencer <morgan@spencercreative.co>
2021-04-17 17:04:54 -0400

    Update AppLayout and add providers to home page

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,48 +1,24 @@
-import Auth0 from 'src/components/Auth0'
-import AzureActiveDirectory from 'src/components/AzureActiveDirectory'
-import Netlify from 'src/components/Netlify'
-import MagicLink from 'src/components/MagicLink'
-import Firebase from 'src/components/Firebase'
-import Supabase from 'src/components/Supabase'
-import Card from 'src/components/Card/Card'
+import { Link, routes } from '@redwoodjs/router'
+import { providers } from 'src/lib/providers'
 
 const HomePage = () => {
   return (
-    <div className="max-w-7xl py-5 mx-auto sm:px-6 lg:px-8">
-      <h1 className="text-lg pb-5 leading-6 font-medium text-gray-900 capitalize">
-        @redwoodjs/auth in action
-      </h1>
-      <p className="text-md pb-5">
-        This page demonstrates authentication providers supported by{' '}
-        <a
-          className="underline"
-          href="https://www.redwoodjs.com/authentication"
-          targer="_blank"
-        >
-          RedwoodJS
-        </a>
-        .
-      </p>
-      <ul className="space-y-3">
-        <Card>
-          <Auth0 />
-        </Card>
-        <Card>
-          <AzureActiveDirectory />
-        </Card>
-        <Card>
-          <Netlify />
-        </Card>
-        <Card>
-          <MagicLink />
-        </Card>
-        <Card>
-          <Firebase />
-        </Card>
-        <Card>
-          <Supabase />
-        </Card>
-      </ul>
+    <div className="max-w-7xl w-full mx-auto sm:px-6 lg:px-8">
+      <div className="grid sm:grid-cols-2 gap-3 md:grid-cols-3">
+        {providers.map((provider, i) => (
+          <Link
+            key={i}
+            to={routes.provider({ provider: provider.slug })}
+            className="col-span-1 flex items-center justify-center py-8 px-8 bg-white border border-red-300 rounded-md transition-colors duration-200 hover:bg-red-100"
+          >
+            <img
+              className="max-h-10 h-full w-auto"
+              src={provider.image}
+              alt={provider.name}
+            />
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
