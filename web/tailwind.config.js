828f0d4700881f013962cde3b23c83e84cc624f7
Morgan Spencer <morgan@spencercreative.co>
2021-04-17 17:03:50 -0400

    Add base styles with Redwood colors

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          100: '#FDF8F6',
+          200: '#FAEAE5',
+          300: '#F3C7BA',
+          400: '#EBA48E',
+          500: '#E38163',
+          600: '#DC5E38',
+          700: '#BF4722',
+          800: '#682712',
+          900: '#341309',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
