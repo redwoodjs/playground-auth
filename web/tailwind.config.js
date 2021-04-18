e5289c563b45eded7e1763c1b32a08c569a1ac4c
Morgan Spencer <morgan@spencercreative.co>
2021-04-18 18:28:26 -0400

    Add Tailwind purge and fix badge style

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/components/**/*.js',
+    './src/layouts/**/*.js',
+    './src/pages/**/*.js',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
