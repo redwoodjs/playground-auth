a3d6d26a65bd37ba095741e7bd80632e5de43f88
Omar El-Domeiri <doesnotexist@users.noreply.github.com>
2021-10-01 12:04:15 -0400

    minimize firebaseConfig needed for admin sdk

--- api/src/lib/auth.js
+++ api/src/lib/auth.js
@@ -12,17 +12,12 @@ import { logger } from './logger'
 
 import admin from 'firebase-admin'
 
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+const firebaseConfig = {
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
 }
 
-admin.initializeApp(config)
+admin.initializeApp(firebaseConfig)
 
 export const getCurrentUser = async (decoded, { type, token }) => {
   logger.debug(
