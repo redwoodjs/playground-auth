afc3721cac36544f634fc76c225964d371445f9f
A. David Thyresson <dthyresson@gmail.com>
2021-12-07 14:01:21 -0500

    Restore custom theme

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        red: {
+          100: '#FDF8F6',
+          200: '#FAEAE5',
+          300: '#F3C7BA',
+          400: '#EBA48E',
+          500: '#E38163',
+          600: '#DC5E38',
+          700: '#BF4722',
+          800: '#682712',
+          900: '#341309',
+        },
+      },
+      fontSize: {
+        '2xs': '.625rem',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
