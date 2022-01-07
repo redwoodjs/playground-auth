d79d1f40eeb7d1c07666bf143c1804fc17a9acbf
Nemi Shah <nemishah1212@gmail.com>
2022-01-07 21:52:31 +0530

    Refactor Session recipe import for SuperTokens

--- web/src/components/SuperTokens/SuperTokens.js
+++ web/src/components/SuperTokens/SuperTokens.js
@@ -3,8 +3,9 @@ import { AuthProvider, useAuth } from '@redwoodjs/auth'
 import ThirdPartyEmailPassword, {SignInAndUp} from "supertokens-auth-react/recipe/thirdpartyemailpassword";
 import UserTools from '../UserTools/UserTools';
 import { RedwoodApolloProvider } from '@redwoodjs/web/dist/apollo'
+import Session from 'supertokens-auth-react/recipe/session'
 
-export const SuperTokensClient = {authRecipe: ThirdPartyEmailPassword};
+export const SuperTokensClient = {authRecipe: ThirdPartyEmailPassword, sessionRecipe: Session};
 
 const Content = (props) => {
   const {isAuthenticated, loading} = useAuth();
