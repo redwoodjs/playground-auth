8616ba19821beca68804dbc6d0805f2649375f68
A. David Thyresson <dthyresson@gmail.com>
2022-02-28 11:20:02 -0500

    Supertokens app changes

--- web/src/App.js
+++ web/src/App.js
@@ -5,55 +5,55 @@ import Routes from 'src/Routes'
 import './index.css'
 import 'highlight.js/styles/atom-one-dark.css'
 
-import SuperTokens from "supertokens-auth-react";
-import Sessions from "supertokens-auth-react/recipe/session";
-import ThirdPartyEmailPassword, {Github, Google, Apple} from "supertokens-auth-react/recipe/thirdpartyemailpassword";
+import SuperTokens from 'supertokens-auth-react'
+import Sessions from 'supertokens-auth-react/recipe/session'
+import ThirdPartyEmailPassword, {
+  Github,
+  Google,
+  Apple,
+} from 'supertokens-auth-react/recipe/thirdpartyemailpassword'
 
 SuperTokens.init({
   appInfo: {
     apiDomain: process.env.SUPERTOKENS_API_DOMAIN,
     websiteDomain: process.env.SUPERTOKENS_WEBSITE_DOMAIN,
     apiGatewayPath: process.env.SUPERTOKENS_API_GATEWAY_PATH,
-    appName: "SuperTokens RedwoodJS",
-    websiteBasePath: "/supertokens",
-    apiBasePath: "/auth",
+    appName: 'SuperTokens RedwoodJS',
+    websiteBasePath: '/supertokens',
+    apiBasePath: '/auth',
   },
   recipeList: [
     Sessions.init(),
     ThirdPartyEmailPassword.init({
       style: {
         button: {
-          backgroundColor: "rgb(191 71 34)",
-          borderColor: "rgb(191 71 34)",
+          backgroundColor: 'rgb(191 71 34)',
+          borderColor: 'rgb(191 71 34)',
         },
       },
       getRedirectionURL: async (context) => {
-        if (context.action === "SUCCESS") {
-            if (context.redirectToPath !== undefined) {
-                // we are navigating back to where the user was before they authenticated
-                return context.redirectToPath;
-            }
-            return "/supertokens";
+        if (context.action === 'SUCCESS') {
+          if (context.redirectToPath !== undefined) {
+            // we are navigating back to where the user was before they authenticated
+            return context.redirectToPath
+          }
+          return '/supertokens'
         }
-        return undefined;
+        return undefined
       },
       signInAndUpFeature: {
         disableDefaultImplementation: true,
         style: {
           container: {
-            width: "auto",
-            boxShadow: "none",
+            width: 'auto',
+            boxShadow: 'none',
           },
         },
-        providers: [
-          Github.init(),
-          Google.init(),
-          Apple.init(),
-        ],
+        providers: [Github.init(), Google.init(), Apple.init()],
       },
-    })
-  ]
-});
+    }),
+  ],
+})
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
