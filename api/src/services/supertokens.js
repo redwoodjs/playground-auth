280738e8e2998558b83db8de47ae5c1426a56dc8
A. David Thyresson <dthyresson@gmail.com>
2022-03-01 08:30:24 -0500

    Support Netlify domains in supertokens service

--- api/src/services/supertokens.js
+++ api/src/services/supertokens.js
@@ -1,16 +1,40 @@
-import ThirdPartyEmailPassword, {Google, Github, Apple} from "supertokens-node/recipe/thirdpartyemailpassword";
-import * as Sessions from "supertokens-node/recipe/session";
+import ThirdPartyEmailPassword, {
+  Google,
+  Github,
+  Apple,
+} from 'supertokens-node/recipe/thirdpartyemailpassword'
+import * as Sessions from 'supertokens-node/recipe/session'
+
+/**
+ * apiDomain setting
+ *
+ * Handles local dev as well as Netlify environment settings for deploy previews and production branches
+ */
+const apiDomain =
+  process.env.BRANCH === 'main'
+    ? process.env.URL
+    : process.env.DEPLOY_URL || process.env.SUPERTOKENS_API_DOMAIN
+
+/**
+ * websiteDomain setting
+ *
+ * Handles local dev as well as Netlify environment settings for deploy previews and production branches
+ */
+const websiteDomain =
+  process.env.BRANCH === 'main'
+    ? process.env.URL
+    : process.env.DEPLOY_URL || process.env.SUPERTOKENS_WEBSITE_DOMAIN
 
 export const config = {
-  framework:"awsLambda",
+  framework: 'awsLambda',
   isInServerlessEnv: true,
   appInfo: {
-    apiDomain: process.env.SUPERTOKENS_API_DOMAIN,
-    websiteDomain: process.env.SUPERTOKENS_WEBSITE_DOMAIN,
+    apiDomain,
+    websiteDomain,
     apiGatewayPath: process.env.SUPERTOKENS_API_GATEWAY_PATH,
-    appName: "SuperTokens RedwoodJS",
-    websiteBasePath: "/supertokens",
-    apiBasePath: "/auth",
+    appName: 'SuperTokens RedwoodJS',
+    websiteBasePath: '/supertokens',
+    apiBasePath: '/auth',
   },
   supertokens: {
     connectionURI: process.env.SUPERTOKENS_CONNECTION_URI,
@@ -20,24 +44,24 @@ export const config = {
       providers: [
         Google({
           clientId: process.env.SUPERTOKENS_GOOGLE_CLIENT_ID,
-          clientSecret: process.env.SUPERTOKENS_GOOGLE_CLIENT_SECRET
+          clientSecret: process.env.SUPERTOKENS_GOOGLE_CLIENT_SECRET,
         }),
         Github({
-            clientId: process.env.SUPERTOKENS_GITHUB_CLIENT_ID,
-            clientSecret: process.env.SUPERTOKENS_GITHUB_CLIENT_SECRET
+          clientId: process.env.SUPERTOKENS_GITHUB_CLIENT_ID,
+          clientSecret: process.env.SUPERTOKENS_GITHUB_CLIENT_SECRET,
         }),
         Apple({
-            clientId: process.env.SUPERTOKENS_APPLE_CLIENT_ID,
-            clientSecret: {
-                keyId: process.env.SUPERTOKENS_APPLE_SECRET_KEY_ID,
-                privateKey: process.env.SUPERTOKENS_APPLE_SECRET_PRIVATE_KEY,
-                teamId: process.env.SUPERTOKENS_APPLE_SECRET_TEAM_ID,
-            },
+          clientId: process.env.SUPERTOKENS_APPLE_CLIENT_ID,
+          clientSecret: {
+            keyId: process.env.SUPERTOKENS_APPLE_SECRET_KEY_ID,
+            privateKey: process.env.SUPERTOKENS_APPLE_SECRET_PRIVATE_KEY,
+            teamId: process.env.SUPERTOKENS_APPLE_SECRET_TEAM_ID,
+          },
         }),
       ],
     }),
     Sessions.init({
-      jwt: {enable: true}
-    })
+      jwt: { enable: true },
+    }),
   ],
 }
