3333ada3f198e3cf57dc110d36d5d257270c9d8d
A. David Thyresson <dthyresson@gmail.com>
2022-03-02 10:09:11 -0500

    Try setting jwksIssuerUrl for Netlify

--- api/src/lib/supertokens.js
+++ api/src/lib/supertokens.js
@@ -36,10 +36,28 @@ const apiBasePath = process.env.NETLIFY
   ? `${process.env.SUPERTOKENS_API_GATEWAY_PATH}/auth`
   : '/auth'
 
+/**
+ * jwksIssuerUrl setting
+ *
+ * Handles local dev as well as Netlify environment settings
+ *
+ * In some cases you may need to provide a custom issuer, for example
+ * during development you may need to test with external services
+ * (like Hasura Cloud). Since the JWKS endpoint is exposed via your backend,
+ * JWT verification will fail because the service may not be able to
+ * query your local environment (localhost, 127.0.0.1).
+ * You can expose your local environment to the internet
+ * (using ngrok for example), and set a custom issuer URL instead
+ *
+ * @see https://supertokens.com/docs/thirdpartyemailpassword/common-customizations/sessions/with-jwt/enabling-jwts#using-a-custom-issuer
+ */
+const jwksIssuerUrl = process.env.NETLIFY ? { issuer: `${apiDomain}/auth` } : {}
+
 console.log(apiDomain, '>>> SuperTokens apiDomain')
 console.log(websiteDomain, '>>> SuperTokens websiteDomain')
 console.log(apiBasePath, '>>> SuperTokens apiBasePath')
 console.log(process.env.CONTEXT, '>>> Netlify process.env.CONTEXT ')
+console.log(jwksIssuerUrl, '>>> SuperTokens jwksIssuerUrl ')
 
 export const config = {
   framework: 'awsLambda',
@@ -76,7 +94,7 @@ export const config = {
       ],
     }),
     Sessions.init({
-      jwt: { enable: true },
+      jwt: { enable: true, ...jwksIssuerUrl },
     }),
   ],
 }
